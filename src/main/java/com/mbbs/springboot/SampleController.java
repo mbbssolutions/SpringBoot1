b59b3027847a54553b8de7862900e1138ac21bf5
Pavan Karasala <karasalapavankumar@gmail.com>
2016-12-04 19:28:28 +0000

    Updated

--- src/main/java/com/mbbs/springboot/SampleController.java
+++ src/main/java/com/mbbs/springboot/SampleController.java
@@ -5,20 +5,34 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Pavan_Karasala on 12/3/2016.
  */
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class SampleController {
 
-    @RequestMapping("/")
+    @RequestMapping("/owner")
     @ResponseBody
     String home(){
         return "Hello Pavan Karasala!!!";
     }
+
+    @RequestMapping("/mbbs")
+    @ResponseBody
+    String home1(){
+        return "Welcome to MBBS solutions !!!";
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home2(){
+        return "Helloo !!!";
+    }
+
 
     public static void main(String args[]){
         SpringApplication.run(SampleController.class,args);
